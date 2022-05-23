b768a22b65d6ddf070501b5508a36d018d0735af
Ujwal Gupta <ujwalgupta@Ujwals-MacBook-Pro.local>
2022-05-23 16:36:04 +0530

    project

---
+++ smart-contract/factory.js
@@ -0,0 +1,9 @@
+import web3 from "web3";
+import CampaignFactory from "./build/CampaignFactory.json";
+
+const instance = new web3.eth.Contract(
+	JSON.parse(CampaignFactory.interface),
+	"0x5433904440c462DF3DB80b20cdC3E35fEcA4424f"
+);
+
+export default instance;
